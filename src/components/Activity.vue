4511bb584d7b9bcee0b62fbe83ec1820424d660e
Goran Radmanovic <goranradmanovic@gmail.com>
2019-09-18 13:18:29 +0200

    Addign API calls for form fileds

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 
   .row
-    b-modal(id='saving-modal' centered title="Data is saved" size='sm' ok-only @ok='getRandomActivity()') Your selection is saved!
-    b-modal(id='error-modal' centered title="Error" size='sm' ok-only @ok='getRandomActivity()') ERROR!!! PLEASE TRY AGAIN!
+    b-modal(id='saving-modal' centered title="Data is saved" size='sm' ok-only @ok='getAPIData(apiLink)') Your selection is saved!
+    b-modal(id='error-modal' centered title="Error" size='md' ok-only @ok='getAPIData(apiLink)')
+      p ERROR!!! PLEASE TRY AGAIN!
+      p {{ this.errorMessage }}
 
     .col-md-7.col-sm-12
       h2.mb-4 You should:
@@ -15,20 +17,20 @@
     .col-md-5.col-sm-12
       h2.mb-4 Activity details:
 
-      b-form-group#fieldset-1(label='Type' label-for='input-1')
-        b-form-select#input-1.text-capitalize(v-model='selected' :options='options' disabled)
+      b-form-group#fieldset-1(label='Type' label-for='select-1')
+        b-form-select#select-1.text-capitalize(v-model='selected' :options='options' ref='selectBox' @change='getType()')
 
-      b-form-group#fieldset-2.mt-3(v-model='participants' label='Participants' label-for='input-2')
-        b-form-input#input-2(v-model='participants' trim placeholder='Enter number of participants' readonly)
+      b-form-group#fieldset-2.mt-3(v-model='participants' label='Participants' label-for='input-1')
+        b-form-input#input-1(v-model='participants' trim placeholder='Enter number of participants' @input='getParticipants()' ref='inputField')
 
       b-form-group#fieldset-3.mt-3(label='Budget' label-for='range-1')
-        b-form-input#range-1(v-model='rangeValue' type='range' min='0' max='5' disabled)
+        b-form-input#range-1(v-model='rangeValue' type='range' min='0' max='10' ref='rangeField' @change='getBudget()')
         .d-flex.justify-content-between
           small.text-secondary cheap
           small.text-secondary expensive
 
       b-form-group
-        b-button(variant='primary' block @click='getRandomActivity()') Hit me with a new one!
+        b-button(variant='primary' block @click='getAPIData(apiLink), resetDisabledFormFileds()') Hit me with a new one!
           b-spinner.ml-3(small label='Loading...' v-if='loading')
 </template>
 
@@ -41,20 +43,32 @@
         participants: '',
         rangeValue: '0',
         selected: null,
-        options: [{value: 'hello', text: 'World'}],
+        options: [
+          {value: 'education', text: 'Education'},
+          {value: 'recreational', text: 'Recreational'},
+          {value: 'social', text: 'Social'},
+          {value: 'diy', text: 'Diy'},
+          {value: 'charity', text: 'Charity'},
+          {value: 'cooking', text: 'Cooking'},
+          {value: 'relaxation', text: 'Relaxation'},
+          {value: 'music', text: 'Music'},
+          {value: 'busywork', text: 'Busywork'},
+        ],
         responseData: null,
         storeData: null,
-        loading: false
+        loading: false,
+        apiLink: 'https://www.boredapi.com/api/activity/',
+        errorMessage: ''
       }
     },
 
     methods: {
 
-       async getRandomActivity() {
+       async getAPIData(url) {
          this.loading = true;
 
          //Fetch the API
-         let response = await fetch('https://www.boredapi.com/api/activity/');
+         let response = await fetch(url);
 
          //If response is 404
          if (response.status == 404) {
@@ -63,6 +77,13 @@
 
          //Get JSON data
          this.responseData = await response.json();
+
+         //If there is no data from API call
+         if (this.responseData.error) {
+           this.apiLink = 'https://www.boredapi.com/api/activity/'; //Set api link to default one
+           this.errorMessage = this.responseData.error; //Set the message
+           this.$bvModal.show('error-modal'); //Show error modal
+         }
 
          //Set the Activity data
          this.setActivityData(this.responseData);
@@ -76,7 +97,6 @@
        setActivityData(data) {
          //Set Activiy page data
          this.randomText = data.activity;
-         this.options.push({value: data.type, text: data.type});
          this.selected = data.type;
          this.participants = data.participants;
          this.rangeValue = data.price * 10;
@@ -88,7 +108,7 @@
            key: data.key,
            activity: data.activity,
            participants: data.participants,
-           budget: (data.price * 10 < 3) ? 'cheap' : 'expensive'
+           budget: ((data.price * 10) < 5) ? 'cheap' : 'expensive'
          }
        },
 
@@ -97,11 +117,56 @@
          //Send data to the vuex store
          this.$store.dispatch('getPersistedData', this.storeData);
          this.$bvModal.hide('saving-modal');
+       },
+
+       //API - Get type of activity
+       getType() {
+         //Disable other form fileds
+         let formEl = document.querySelectorAll('#input-1, #range-1');
+         formEl[0].disabled = true;
+         formEl[1].disabled = true;
+
+         //Create new API link
+         this.apiLink = `https://www.boredapi.com/api/activity?type=${this.selected}`;
+       },
+
+       //API - Get participants of activity
+       getParticipants() {
+         //Disable other form fileds
+         let formEl = document.querySelectorAll('#select-1, #range-1');
+         formEl[0].disabled = true;
+         formEl[1].disabled = true;
+
+         //Create new API link
+         this.apiLink = `https://www.boredapi.com/api/activity?participants=${this.participants}`;
+       },
+
+       //API - Get budget of activity
+       getBudget() {
+         //Get form fileds
+         let formEl = document.querySelectorAll('#select-1, #input-1'),
+             formatedRangeValue = this.rangeValue / 10; //Range from 0.0 - 1 (this.rangeValue is whole number 0 - 10)
+
+         //Disable other form fileds
+         formEl[0].disabled = true;
+         formEl[1].disabled = true;
+
+         //Create new API link
+         this.apiLink = `https://www.boredapi.com/api/activity?price=${formatedRangeValue}`;
+       },
+
+       resetDisabledFormFileds() {
+         let formEl = document.querySelectorAll('#select-1, #input-1, #range-1');
+         //Undisable other form fileds
+         formEl[0].disabled = false;
+         formEl[1].disabled = false;
+         formEl[2].disabled = false;
        }
     },
 
     created() {
-      this.getRandomActivity();
+      //Initial request for random activity data
+      this.getAPIData(this.apiLink);
     }
   }
 </script>
